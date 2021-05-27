9aac5f8471bdcb3d781579a8419bcb96150b98e3
Murph Murphy <murph@clurictec.com>
2021-05-27 16:23:50 -0600

    Pre depolyment

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -7,16 +7,18 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Router, Route, Set } from '@redwoodjs/router'
+import { Router, Route, Set, Private } from '@redwoodjs/router'
 import BlogLayout from './layouts/BlogLayout/BlogLayout'
 
 const Routes = () => {
   return (
     <Router>
-      <Route path="/admin/posts/new" page={NewPostPage} name="newPost" />
-      <Route path="/admin/posts/{id}/edit" page={EditPostPage} name="editPost" />
-      <Route path="/admin/posts/{id}" page={PostPage} name="post" />
-      <Route path="/admin/posts" page={PostsPage} name="posts" />
+      <Private unauthenticated="home">
+        <Route path="/admin/posts/new" page={NewPostPage} name="newPost" />
+        <Route path="/admin/posts/{id:String}/edit" page={EditPostPage} name="editPost" />
+        <Route path="/admin/posts/{id:String}" page={PostPage} name="post" />
+        <Route path="/admin/posts" page={PostsPage} name="posts" />
+      </Private>
       <Set wrap={BlogLayout}>
         <Route path="/contact" page={ContactPage} name="contact" />
         <Route path="/blog-post/{id:String}" page={BlogPostPage} name="blogPost" />
--- web/src/layouts/BlogLayout/BlogLayout.tsx
+++ web/src/layouts/BlogLayout/BlogLayout.tsx
@@ -1,6 +1,9 @@
+import { useAuth } from '@redwoodjs/auth'
 import { Link, routes } from '@redwoodjs/router'
 
 const BlogLayout: React.FunctionComponent = ({ children }) => {
+  const { logIn, logOut, isAuthenticated, currentUser } = useAuth()
+
   return (
     <>
       <header>
@@ -15,6 +18,12 @@ const BlogLayout: React.FunctionComponent = ({ children }) => {
             <li>
               <Link to={routes.contact()}>Contact</Link>
             </li>
+            <li>
+              <button onClick={isAuthenticated ? logOut : logIn}>
+                {isAuthenticated ? 'Log Out' : 'Log In'}
+              </button>
+            </li>
+            {isAuthenticated && <li>{currentUser.email}</li>}
           </ul>
         </nav>
       </header>
